aba57f1c0f638aa0b423b151870fa81bd601757f
mollymilesgit <mollymgit@gmail.com>
2024-06-13 01:38:16 -0600

    Add button to change size of grid index.html and javascript.js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,27 @@
 
 const containerDiv = document.querySelector("#container")
+const gridButton = document.querySelector(".sizeChooser")
+let gridSizeTrue = 16
+gridButton.onclick = () => {
+    let gridSizeQuery = prompt("Please enter a size for the etch-a-sketch!")
+        if (gridSizeQuery <= 100 && gridSizeQuery > 0) {
+            gridSizeTrue = gridSizeQuery
+            divCreation();
+        }
+        else {
+           return alert("Too big,negative, or not a number")
+}
+}
 function divCreation() {
+    containerDiv.replaceChildren();
     let a = 0
-    while (a <= 16) {
+    while (a < gridSizeTrue) {
     let i = 0
     const divRow = document.createElement("div")
     divRow.classList.add("rowdiv")
     containerDiv.appendChild(divRow);
     a++
-        while (i <= 16) {
+        while (i < gridSizeTrue) {
         const innerDiv = document.createElement("div")
         innerDiv.classList.add("innerdiv")
         innerDiv.onmouseover = () => {
